5b383ed71b567499804eec6d2a2bec4e8ee0c7a8
Jyanial Dewan <djyanial@gmail.com>
2026-01-11 15:24:37 +0600

    location upsert

--- Routes/index.js
+++ Routes/index.js
@@ -1,10 +1,12 @@
 const Router = require("express");
-const usersRoute = require("./usersRoutes");
 const authRoute = require("./authRoutes");
+const usersRoute = require("./usersRoutes");
+const locationRoute = require("./locationRoutes");
 
 const routes = Router();
 
-routes.use("/api/users", usersRoute);
 routes.use("/api/auth", authRoute);
+routes.use("/api/users", usersRoute);
+routes.use("/api/location", locationRoute);
 
 module.exports = routes;
